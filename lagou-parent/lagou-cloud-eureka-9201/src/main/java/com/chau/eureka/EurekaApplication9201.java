38b38eb3083d12158f7654648e103be4c72f7c0c
wilfred <724091702@qq.com>
2022-07-01 18:43:16 +0800

    模拟Eureka Server集群

---
+++ lagou-parent/lagou-cloud-eureka-9201/src/main/java/com/chau/eureka/EurekaApplication9201.java
@@ -0,0 +1,20 @@
+package com.chau.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: wilfred
+ * @CreateTime: 2022-07-01  18:34
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication9201 {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EurekaApplication9201.class, args);
+    }
+}
